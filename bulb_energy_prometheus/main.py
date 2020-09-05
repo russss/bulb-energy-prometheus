bc137f4bb05abe61fa3e3e450de86488c0c8d0be
Russ Garrett <russ@garrett.co.uk>
2020-09-05 11:05:12 +0100

    Add resilience against readings going backwards or to zero
    Fixes #2, hopefully

--- bulb_energy_prometheus/main.py
+++ bulb_energy_prometheus/main.py
@@ -5,9 +5,12 @@ import asyncio
 import aiohttp
 import pysmartthings
 
-gauge_electricity = Gauge("bulb_electricity_used_kwh", "Electricity meter reading")
-gauge_gas = Gauge("bulb_gas_used_units", "Gas meter reading")
-refresh_time = Summary("smartthings_refresh_time_seconds", "Time taken to fetch data from SmartThings")
+# Newly-initialised gauges are set to 0, which is bad. Only create the gauge once we have a valid reading.
+gauge_electricity = None
+gauge_gas = None
+refresh_time = Summary(
+    "smartthings_refresh_time_seconds", "Time taken to fetch data from SmartThings"
+)
 
 
 async def get_device(api):
@@ -17,7 +20,22 @@ async def get_device(api):
     return None
 
 
+def valid_reading(reading, previous_reading):
+    """ Check if a reading is valid.
+
+        The SmartThings API may return zero or the reading may go backwards, which confuses things.
+        Ensure that we have a valid, increasing reading here.
+    """
+    return (
+        reading is not None
+        and reading > 0
+        and (previous_reading is None or reading >= previous_reading)
+    )
+
+
 async def main(api_token):
+    global gauge_electricity, gauge_gas
+
     async with aiohttp.ClientSession() as session:
         api = pysmartthings.SmartThings(session, api_token)
         device = await get_device(api)
@@ -25,19 +43,38 @@ async def main(api_token):
             print("Can't find energy monitor device")
             return
 
+        gas_reading = None
+        electricity_reading = None
+
         print("Connected, running...")
         while True:
             with refresh_time.time():
                 await device.status.refresh()
-            if device.status.values.get("energy"):
-                gauge_electricity.set(device.status.values.get("energy"))
-            else:
-                print("Can't get electricity reading")
 
-            if device.status.values.get("gasMeter"):
-                gauge_gas.set(device.status.values.get("gasMeter"))
+            new_electricity = device.status.values.get("energy")
+            if valid_reading(new_electricity, electricity_reading):
+                electricity_reading = new_electricity
             else:
-                print("Can't get gas meter reading")
+                print(
+                    f"Invalid electricity reading: {new_electricity} (previous:"
+                    f" {electricity_reading})"
+                )
+
+            new_gas = device.status.values.get("gasMeter")
+            if valid_reading(new_gas, gas_reading):
+                gas_reading = new_gas
+            else:
+                print(f"Invalid gas reading: {new_gas} (previous: {gas_reading})")
+
+            if electricity_reading:
+                if gauge_electricity is None:
+                    gauge_electricity = Gauge("bulb_electricity_used_kwh", "Electricity meter reading")
+                gauge_electricity.set(electricity_reading)
+
+            if gas_reading:
+                if gauge_gas is None:
+                    gauge_gas = Gauge("bulb_gas_used_units", "Gas meter reading")
+                gauge_gas.set(gas_reading)
 
             await asyncio.sleep(10)
 
@@ -47,7 +84,8 @@ def run():
 
     if not os.environ.get("SMARTTHINGS_API_TOKEN"):
         print(
-            "SmartThings API Token should be provided in the SMARTTHINGS_API_TOKEN environment variable."
+            "SmartThings API Token should be provided in the SMARTTHINGS_API_TOKEN"
+            " environment variable."
         )
         sys.exit(1)
 
